5d922f45e75593542daa5123464af2808603b0e3
Andre Richardi <a.richardi@gmail.com>
2021-01-24 12:14:11 -0300

    my first commit - starting new study

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require('mongoose');
+
+const app = express();
+
+//Middleware que substitiu o body parser e que lê o json no body do post
+app.use(express.json());
+
+//Config of DB
+//import the keyconfig and link to the mongoURL
+const db = require('./config/keys').mongoURL;
+
+//use that key to connect in mongoose
+//o retorno é uma promise, and you can send a console back to visualize the feedback
+mongoose
+    .connect(db)
+    .then( () => console.log('MongoDB is connected ...'))
+    .catch( (err) => console.log(err) );
+
+//config the port that server will use. The enviroment port or 5000
+const port = process.env.PORT || 5000;
+
+//rodar o servidor
+app.listen(port, () => console.log(`The server is running on port ${port}...`));
