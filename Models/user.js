c8879b47dbd3505f3cdb3367b0d7e54fefd9dd14
Andre Richardi <a.richardi@gmail.com>
2021-02-08 08:35:41 -0300

    new model user

---
+++ Models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+//create our Schema
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    register_date:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+//export the models, mongoose.model(name, schema const)
+module.exports  = User = mongoose.model('user', UserSchema)
